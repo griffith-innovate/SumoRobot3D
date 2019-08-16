d4287d2941a3c4e336af332ee3fca470804e3072
ryomaohira <r.j.ohira@gmail.com>
2019-08-16 23:04:47 +1000

    Added line sensors. The Dohyo needs two collider meshes. 1 for the main ring and another for the border line. LineSensor activates by ray-casting and detecting what object it hits.

---
+++ Assets/Components/Sensors/LineSensor/LineSensor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineSensor : MonoBehaviour
+{
+    public float Range = 0.1f;
+    private RaycastHit vision;          // Used for detecting Raycast collision
+    public bool hit = false;                
+    // public Rigidbody hitObject;
+    public Collider hitObject;
+    private float angle;
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hit = false;
+        angle = transform.rotation.eulerAngles.y;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Reset values
+        hit = false;
+        hitObject = null;
+        distance = -1.0f;
+
+        // Visualise the raycast in the scene
+        Debug.DrawRay(transform.position, transform.forward * Range, Color.yellow, 0.5f);
+        
+        // Do something if the raycast hits a rigid body in the scene
+        if (Physics.Raycast(transform.position, transform.forward, out vision, Range))
+        {
+            // Output the name of the object the raycast has collided with
+            // Debug.Log(vision.collider.name + ": " + vision.distance);
+            // hit = true;
+            hitObject = vision.collider;
+            distance = vision.distance;
+            if(hitObject.name == "WhiteLine"){
+                hit = true;
+            }
+            // Debug.Log(hitObject);
+        }
+        
+    }
+    
+    public float Angle(){
+        return angle;
+    }
+    public float Distance(){
+        return distance;
+    }
+    public Collider HitObject(){
+        return hitObject;
+    }
+    public bool Hit(){
+        return hit;
+    }
+}
--- Assets/Robots/BasicRobot/DumbAgent.cs
+++ Assets/Robots/BasicRobot/DumbAgent.cs
@@ -6,51 +6,124 @@ public class DumbAgent : MonoBehaviour
 {
     private RobotController Controller;
     public Collider Target;
-    public OpticalSensor FoundBy;
+    public OpticalSensor TargetFoundBy;
+    public List<LineSensor> LineFoundBy;
+    public bool isAlive = true;
+    public string LineDirection;
+    public float avgX;
+    public float avgZ;
+    public List<float> xValues;
+    public List<float> zValues;
+    private bool turning;
 
     // Start is called before the first frame update
     void Start()
     {
         Controller = FindObjectOfType(typeof(RobotController)) as RobotController;
         Target = null;
-        FoundBy = null;
+        TargetFoundBy = null;
+        LineFoundBy = new List<LineSensor>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Scan();
-        if(Target == null){
-            Controller.SetSpeed(0.0f);
-            Scout();
+        // Only do something if it's alive
+        if(isAlive){
+            Scan();
+            CheckLineSensors();
+            if(Target == null){
+                Controller.SetSpeed(1.0f);
+                Scout();
+            } else {
+                Aim();
+                Controller.SetSpeed(1.0f);
+            }
+        }
+    }
+
+    void CheckLineSensors(){
+        Debug.Log("CheckLineSensors()");
+        // Clear our line sensor data
+        LineFoundBy.Clear();
+
+        foreach(LineSensor sensor in Controller.LineSensors){
+            if(sensor.Hit()){
+                LineFoundBy.Add(sensor);        
+            }
+        }
+
+        // What direction is the line?
+        LineDirection = "Test";
+        float sumX = 0.0f;
+        float sumZ = 0.0f;
+        // float avgX, avgZ;
+        foreach(LineSensor sensor in LineFoundBy) {
+            Vector3 relativePoint = transform.InverseTransformPoint(sensor.transform.position);
+            sumX += (float)System.Math.Round(relativePoint.x, 3);
+            sumZ += (float)System.Math.Round(relativePoint.z, 3);
+            xValues.Add(relativePoint.x);
+            zValues.Add(relativePoint.z);
+        }
+        avgX = sumX / LineFoundBy.Count;
+        avgZ = sumZ / LineFoundBy.Count;
+        if(avgZ > 0) {
+            LineDirection = "North";
+        } else if(avgZ < 0) {
+            LineDirection = "South";
         } else {
-            Aim();
-            Controller.SetSpeed(1.0f);
+            LineDirection = "Middle";
+        }
+        if(avgX > 0) {
+            LineDirection += " East";
+        } else if(avgX < 0) {
+            LineDirection += " West";
+        } else {
+            LineDirection += " Center";
         }
     }
 
     void Scan(){
         // Assume we lost them
         Target = null;
-        FoundBy = null;
+        TargetFoundBy = null;
 
         // Find out what it sees
         foreach(OpticalSensor sensor in Controller.OpticalSensors){
             if(sensor.Hit()){
                 Target = sensor.HitObject();
-                FoundBy = sensor;
+                TargetFoundBy = sensor;
                 break;
             }
         }
     }
     void Scout(){
+        // Check to see if our robot has hit a white line
+        if(turning){
+            if(LineFoundBy.Count == 0){
+                turning = false;
+            }
+        } else {
+            if(LineFoundBy.Count > 0){
+                turning = true;
+                if(LineDirection.Contains("East")){
+                    Controller.Turn(-30);
+                } else if(LineDirection.Contains("West")){
+                    Controller.Turn(30);
+                } else {
+                    Controller.Turn(130);
+                }
+            }
+        }
         // Turn around
-        Controller.Turn(-10f);
+            // Controller.Turn(-10f);
+
+
     }
 
     void Aim(){
         // What is the angle of the sensor that found it
-        float angle = FoundBy.Angle();
+        float angle = TargetFoundBy.Angle();
         Controller.Turn(angle);
     }
 }
