b95b5d2caf3694cfec7ed1d607c4d171a1a5588d
SanityLacking <SanityLacking@users.noreply.github.com>
2019-08-16 15:07:28 +1000

    added reset options to bot.

--- Assets/Robots/BasicRobot/RobotController.cs
+++ Assets/Robots/BasicRobot/RobotController.cs
@@ -1,4 +1,4 @@
-﻿// This is essentially the motor controller simulator
+// This is essentially the motor controller simulator
 
 using System.Collections;
 using System.Collections.Generic;
@@ -16,8 +16,11 @@ public class RobotController : MonoBehaviour
     public OpticalSensor[] OpticalSensors;
     public float Force;
     // public MotorController MC;
+    public bool PlayerControled;
 
-
+    public bool setStartLocation;
+    private Vector3 startLocation;
+    private Vector3 startRotation;
     #endregion
     // Start is called before the first frame update
     void Start()
@@ -26,6 +29,14 @@ public class RobotController : MonoBehaviour
         Weight = 3.0f;
         MagneticDownforce = 1000;
         m_Speed = 0.0f;
+
+        // update the reset location of the robot, if not checked, robot will default to zero/zero
+        if (setStartLocation){
+            Debug.Log("updating start position");
+            startLocation = m_Rigidbody.transform.position;
+            startRotation = m_Rigidbody.transform.eulerAngles;
+        }
+
 
         // Get the wheels
         Wheels = GetComponentsInChildren<Wheel>();
@@ -42,6 +53,7 @@ public class RobotController : MonoBehaviour
         // Move object
         // m_Rigidbody.velocity = transform.forward * (Velocity + Time.deltaTime);
         m_Rigidbody.transform.Translate(Vector3.forward * Velocity * Time.deltaTime);
+        AgentReset();
         // m_Rigidbody.transform.forward 
     }
 
@@ -71,37 +83,68 @@ public class RobotController : MonoBehaviour
 
     void CheckInputs()
     {
-        // Check for acceleration
-        if (Input.GetKeyDown("w") || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            // m_Speed += m_Acceleration;
-            //SetSpeed(1);
-            SetSpeed(m_Speed + 0.1f);
-        }
+        if (PlayerControled == true){
+            // Check for acceleration
+            if (Input.GetKeyDown("w") || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                // m_Speed += m_Acceleration;
+                //SetSpeed(1);
+                SetSpeed(m_Speed + 0.1f);
+            }
 
-        // Check for deceleration/braking
-        if (Input.GetKeyDown("s") || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            // m_Speed -= m_Brake;
-            SetSpeed(m_Speed - 0.1f);
-        }
+            // Check for deceleration/braking
+            if (Input.GetKeyDown("s") || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                // m_Speed -= m_Brake;
+                SetSpeed(m_Speed - 0.1f);
+            }
 
-        // Check for right turn
-        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Rotate(0.0f, 0.5f, 0.0f);
-        }
+            // Check for right turn
+            if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(0.0f, 0.5f, 0.0f);
+            }
 
-        // Check for left turn
-        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Rotate(0.0f, -0.5f, 0.0f);
+            // Check for left turn
+            if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(0.0f, -0.5f, 0.0f);
+            }
         }
-
     }
 
     public bool CheckWheelSlip(){
         return false;
+    }
+
+
+    public void AgentReset()
+    {
+        // Debug.Log(targetTransform);
+        if (this.transform.position.y < 0)
+        {
+            // If the Agent fell, zero its momentum
+            Debug.Log("reset Bot");
+            m_Rigidbody.angularVelocity = Vector3.zero;
+            m_Rigidbody.velocity = Vector3.zero;
+            m_Speed =0;
+            SetSpeed(m_Speed);
+            // m_Acceleration = 0;
+            m_Rigidbody.transform.position = startLocation;
+            m_Rigidbody.transform.eulerAngles = startRotation;
+        }
+
+
+        // targetRigidBody.angularVelocity = Vector3.zero;
+        // targetRigidBody.velocity = Vector3.zero;
+        // Move the target to a new spot
+        //generate new position: 
+        // Vector3 newPos = new Vector3(0f,0.5f,0f);
+        // Vector3 newPos = new Vector3(Random.value * 1.22f - 0.64f,0.5f,Random.value * 1.22f - 0.64f);
+        // while (Vector3.Distance(this.transform.position,newPos) < 0.30f){
+        //     newPos = new Vector3(Random.value * 1.22f - 0.64f,0.5f,Random.value * 1.22f - 0.64f);
+        // }
+        // targetRigidBody.transform.position = startLocation;
     }
 
     // Set the speeds of the wheels so that we can have a differential turn
