0dd11925f3a0738e2a1a28f52e739d4fef22b69f
Ryoma Ohira <r.j.ohira@gmail.com>
2019-08-20 12:42:22 +1000

    Added GameController

---
+++ Assets/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+================================================================================'
+GameController
+This script is responsible for managing an array of Sumo games. Each game will 
+have two robots with different neural networks. GameController will reward the
+robots of each game according to their performance and progress to their goals.
+================================================================================
+*/
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
