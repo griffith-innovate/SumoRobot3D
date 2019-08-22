94fc41b0ad74ec05142f478e976c1770224ac3e4
Ryoma Ohira <r.j.ohira@gmail.com>
2019-08-22 18:30:57 +1000

    Added detailed hitboxes to basic robot prefab

--- Assets/Robots/BasicRobot/AgentScript.cs
+++ Assets/Robots/BasicRobot/AgentScript.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using MLAgents;
 
-public class AgentScript : Agent
-{
+public class AgentScript : Agent {
     float speedRewardThreshold = 5.0f;
     RobotController robotController;
     // Rigidbody rBody;
@@ -12,22 +11,20 @@ public class AgentScript : Agent
     Rigidbody targetBody;
 
     Rigidbody m_Rigidbody;
-    void Start () {
+    void Start() {
         m_Rigidbody = GetComponent<Rigidbody>();
-        robotController = GetComponent<RobotController>(); 
+        robotController = GetComponent<RobotController>();
         // target = transform.parent;
         // Debug.Log();
         targetBody = transform.parent.Find("target").GetComponent<Rigidbody>();
     }
     // public Transform targetTransform;
     // public Rigidbody targetRigidBody;
-    public override void AgentReset()
-    {
+    public override void AgentReset() {
         robotController.AgentResetCheck();
     }
 
-    public override void CollectObservations()
-    {
+    public override void CollectObservations() {
         // targetTransform and Agent positions
         AddVectorObs(targetBody.transform.position.x);
         AddVectorObs(targetBody.transform.position.y);
@@ -38,57 +35,54 @@ public class AgentScript : Agent
         AddVectorObs(m_Rigidbody.transform.position.z);
         // Agent velocity
         AddVectorObs(robotController.Speed);
-        AddVectorObs(NormalizeAngle(m_Rigidbody.transform.eulerAngles[1]));        
+        AddVectorObs(NormalizeAngle(m_Rigidbody.transform.eulerAngles[1]));
     }
 
     // private float lastDistFromCenter = 0;
-    public override void AgentAction(float[] vectorAction, string textAction)
-    {
+    public override void AgentAction(float[] vectorAction, string textAction) {
         // Debug.Log("Updated");
         // Actions, size = 2
         Vector3 controlSignal = Vector3.zero;
         float newSpeed = vectorAction[0];
         float newRotation = vectorAction[1];
 
-        Debug.Log("SetSpeed: "+ newSpeed + " SetRotation: "+newRotation);
+        Debug.Log("SetSpeed: " + newSpeed + " SetRotation: " + newRotation);
 
         // m_Rigidbody.AddForce(controlSignal * Speed );
         robotController.SetSpeed(newSpeed);
         robotController.Turn(newRotation);
-       
-       
-       
+
+
+
         //*********** Rewards **************/
-        
+
         //Reward Fast Speed:
         // if (robotController.Speed ){
-            
+
         // }
-        var damping =2;
+        var damping = 2;
         var lookPos = targetBody.transform.position - transform.position;
         lookPos.y = 0;
         var rotation = Quaternion.LookRotation(lookPos);
-        Debug.Log("rotation: "+rotation);
+        Debug.Log("rotation: " + rotation);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
-        
-        
-        if( robotController.Speed > 0 ){
+
+
+        if (robotController.Speed > 0) {
             SetReward(Mathf.Abs(robotController.Speed * 0.1f));
             // SetReward(Mathf.Abs(robotController.DistanceMoved * 0.05f));
         }
-        
-        
-                
+
+
+
         // SetReward(robotController.Speed * 0.1f);
         // Target Fell off platform
-        if (targetBody.transform.position.y < -1.0f || targetBody.transform.position.y > 2.0f)
-        {
+        if (targetBody.transform.position.y < -1.0f || targetBody.transform.position.y > 2.0f) {
             SetReward(50.0f);
             Done();
         }
         // Itself Fell off platform
-        if (m_Rigidbody.transform.position.y < -1.0f || m_Rigidbody.transform.position.y > 2.0f)
-        {
+        if (m_Rigidbody.transform.position.y < -1.0f || m_Rigidbody.transform.position.y > 2.0f) {
             SetReward(-50.0f);
             Done();
         }
@@ -96,12 +90,11 @@ public class AgentScript : Agent
         float distanceToTarget = Vector3.Distance(targetBody.transform.position, transform.position);
         Debug.Log(distanceToTarget);
         // Reached target
-        if (distanceToTarget < 0.40f)
-        {
+        if (distanceToTarget < 0.40f) {
             SetReward(0.2f);
             // Done();
         }
-        
+
         // Debug.Log(m_Rigidbody.velocity.magnitude);
 
         // // if
@@ -125,7 +118,7 @@ public class AgentScript : Agent
         //     // Done();
         // }
 
-       
+
         // // if the target fell off the platform
         // if (targetRigidBody.transform.position.y < 0)
         // {
@@ -134,11 +127,11 @@ public class AgentScript : Agent
         // }
     }
 
-     float NormalizeAngle(float value, float max =360){
-        if (max == 0)  
-            return 0; 
-        else{
-            return  (value / max);
+    float NormalizeAngle(float value, float max = 360) {
+        if (max == 0)
+            return 0;
+        else {
+            return (value / max);
         }
     }
 }
