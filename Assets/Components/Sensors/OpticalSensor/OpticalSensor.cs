8120821fdd4123ece0b7073a793c8c1ab9bd4929
Ryoma Ohira <r.j.ohira@gmail.com>
2019-08-22 16:10:29 +1000

    Issue #8 - Fixed by deleting OpticalSensor.cs and removing the LineOfSight.cs from the prefab. Creating a new OpticalSensor.cs file and attaching to the prefab seems to get it working again.

--- Assets/Components/Sensors/OpticalSensor/OpticalSensor.cs
+++ Assets/Components/Sensors/OpticalSensor/OpticalSensor.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpticalSensor : MonoBehaviour
-{
+public class OpticalSensor : MonoBehaviour {
     #region Public Interfaces
-    public float RayLength { get; set; }                                        // Assign length to the raycast
+    public float RayLength { get; set; } = 4.0f;                                // Assign length to the raycast
     [SerializeField]
     private float rayLength;
     public bool Hit { get; set; } = false;
@@ -28,24 +27,24 @@ public class OpticalSensor : MonoBehaviour
     private RaycastHit vision;          // Used for detecting Raycast collision
 
     // Start is called before the first frame update
-    void Start(){
+    void Start() {
         RayLength = 4.0f;
         Hit = false;
         Angle = transform.rotation.eulerAngles.y;
     }
 
     // Update is called once per frame
-    void Update(){
+    void Update() {
         // Reset values
         Hit = false;
         HitObject = null;
         Distance = -1.0f;
 
         // Visualise the raycast in the scene
-        Debug.DrawRay( transform.position, transform.forward * RayLength, Color.red, 0.5f );
-        
+        Debug.DrawRay(transform.position, transform.forward * RayLength, Color.red, 0.5f);
+
         // Do something if the raycast hits a rigid body in the scene
-        if ( Physics.Raycast( transform.position, transform.forward, out vision, RayLength ) ){
+        if (Physics.Raycast(transform.position, transform.forward, out vision, RayLength)) {
             Hit = true;
             HitObject = vision.collider;
             Distance = vision.distance;
